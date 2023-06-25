ddcd17b3810de47d3d65f8923a890fd2b206f48e
Kavirubc <hapuarachchikaviru@gmail.com>
2023-06-25 10:49:51 +0530

    Calculater

--- test1.c
+++ test1.c
@@ -1,1 +1,46 @@
-@@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // Defining variable
+
+    float num1, num2, cal;
+    char op;
+
+    // Getting inputs and assigning them
+    printf("Enter the operator: ");
+    scanf("%c", &op);
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    // Calculations
+
+    if (op == '/')
+    {
+        cal = num1 / num2;
+        printf("%f", cal);
+    }
+    else if (op == '*')
+    {
+        cal = num1 * num2;
+        printf("%f", cal);
+    }
+    else if (op == '+')
+    {
+        cal = num1 + num2;
+        printf("%f", cal);
+    }
+    else if (op == '-')
+    {
+        cal = num1 - num2;
+        printf("%f", cal);
+    }
+    else
+    {
+        printf("Invalid command or operation");
+    }
+}
