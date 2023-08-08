6f53c01c7a8302ac19c21c6f03a0e622fc4159c2
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2023-08-08 21:16:34 +0530

    palindromeWithoutS.c

--- palindromeWithoutS.c
+++ palindromeWithoutS.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
-
-
 int main()
 {
-    int n;
-    printf("Enter the number of letter: ");
-    scanf("%d", &n);
-    char word[n];
-    printf("Enter the word: ");
-    scanf("%c", &word[n]);
-    printf("%c", word[n]);
+    char word[] = {"cevic"};
+    int flag, num = (sizeof(word) / sizeof(word[0]) - 1);
+    printf("%d\n", num);
+    for (int i = 0; i < num; i++)
+    {
+        if (word[i] != word[num - i - 1])
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+    {
+        printf("Not a palindrome");
+    }
+    else
+    {
+        printf("Word is a Palindorme");
+    }
+
     return 0;
 }
